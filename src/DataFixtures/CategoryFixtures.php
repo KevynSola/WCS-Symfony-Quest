6e56d86aa206d6c86607721e7aa4c47458503b62
Kevyn SOLA <kevyn.sola@gmail.com>
2022-11-29 10:53:39 +0100

    add fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public const CATEGORIES = [
+        'Action',
+        'Science-fiction',
+        'Horreur',
+        'Aventure',
+        'Comédie',
+        'Thriller',
+        'Animation',
+    ];
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}

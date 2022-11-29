3cc9a015740712108ae229f22547f34468502e26
Kevyn SOLA <kevyn.sola@gmail.com>
2022-11-29 15:23:07 +0100

    clean fix

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,19 +2,21 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(Environment $twig, ProgramRepository $programRepository): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
-         ]);
+        return new Response ($twig->render('program/index.html.twig', [
+            'programs' => $programRepository->findAll(),
+         ]));
     }
 
     #[Route('/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'show')]
---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $program = new Program();
+        $program->setTitle('Wednesday');
+        $program->setSynopsis('La série sur la fille de la famille Adams');
+        $program->setCategory($this->getReference('category_Action'));
+        $manager->persist($program);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
+        return [
+          CategoryFixtures::class,
+        ];
+    }
+}

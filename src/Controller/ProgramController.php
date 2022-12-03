4a9acb617a9a24ab28caf648cd039fb04cae04ab
Kevyn SOLA <kevyn.sola@gmail.com>
2022-12-03 18:32:57 +0100

    quest11

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,18 +16,38 @@ class ProgramController extends AbstractController
     {
         return $this->render('program/index.html.twig', [
             'programs' => $programRepository->findAll(),
-         ]);
+        ]);
     }
 
-    #[Route('/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'show')]
+    #[Route('/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'show')]
     public function show(int $id, ProgramRepository $programRepository): Response
     {
         $program = $programRepository->findOneBy(
             ['id' => $id],
         );
-        
+
+        $seasons = $program->getSeasons();
+
         return $this->render('program/show.html.twig', [
             'program' => $program,
+            'seasons' => $seasons,
+        ]);
+    }
+
+    #[Route('/{programId}/seasons/{seasonId}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'season_show')]
+    public function showSeason(int $programId, int $seasonId, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
+    {
+        $program = $programRepository->findOneBy(
+            ['id' => $programId],
+        );
+
+        $season = $seasonRepository->findOneBy(
+            ['id' => $seasonId],
+        );
+
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
         ]);
     }
 }

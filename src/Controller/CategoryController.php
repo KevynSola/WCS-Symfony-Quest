0b9b8429ae163d27d9cacbc6aa4699e80570701b
Kevyn SOLA <kevyn.sola@gmail.com>
2022-12-03 23:49:25 +0100

    add new form to cat and prog

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Repository\CategoryRepository;
 use App\Repository\ProgramRepository;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CategoryType;
 
 #[Route('/category', name: 'category_' )]
 class CategoryController extends AbstractController
@@ -16,6 +19,25 @@ class CategoryController extends AbstractController
     {
         return $this->render('category/index.html.twig', [
             'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name:'new')]
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $categoryRepository->save($category, true);
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->renderForm('category/new.html.twig', [
+            'form' => $form,
         ]);
     }
 
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,13 +2,15 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Entity\Season;
 use App\Entity\Episode;
 use App\Entity\Program;
-use App\Entity\Season;
+use App\Form\ProgramType;
 use App\Repository\ProgramRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
@@ -21,13 +23,31 @@ class ProgramController extends AbstractController
         ]);
     }
 
-    #[Route('/program/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'show')]
-    public function show(int $id, Program $program, ProgramRepository $programRepository): Response
+    #[Route('/new', name:'new')]
+    public function new(Request $request, ProgramRepository $programRepository): Response
+    {
+        $program = new Program();
+
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $programRepository->save($program, true);
+
+            return $this->redirectToRoute('program_index');
+        }
+
+        return $this->renderForm('program/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{program}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'show')]
+    public function show(Program $program, ProgramRepository $programRepository): Response
     {
         $seasons = $program->getSeasons();
 
         return $this->render('program/show.html.twig', [
-            'id' => $id,
             'program' => $program,
             'seasons' => $seasons,
         ]);
